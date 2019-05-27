a08931cd2ad31dd4f3ef87f79a113eb8ed0b4621
lpli <leep123@qq.com>
2019-05-28 00:01:21 +0800

    车系控件

--- src/components/car-selector.vue
+++ src/components/car-selector.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="car-selector">
+  <div class="car-selector" ref="car-selector">
     <el-input type="text" suffix-icon="el-icon-third-down"></el-input>
     <div class="category-div">
       <div class="letter-div">
-        <div class="letter-i" v-for="item in letters" :key="item">{{item}}</div>
+        <div
+          class="letter-i"
+          v-for="item in letters"
+          :key="item"
+          @click="changeLetter(item)"
+        >{{item}}</div>
       </div>
       <div class="band-div">
-          <div class="band-i" v-for="item in letters" :key="item" v-if="bands[item]">
-              <div class="band-c">{{item}}</div>
-              <div class="band-t" v-for="e in Object.keys(bands[item])" :key="e"> 
-                  {{e}}
-              </div>
-          </div>
+        <div class="band-i" v-for="item in letters" :key="item" v-if="brands[item]">
+          <div class="band-c" :data-c="item">{{item}}</div>
+          <div class="band-t" v-for="e in brands[item]" :key="e" @click="getModel(e)">{{e}}</div>
+        </div>
       </div>
-      <div class="model-div">
-          <div v-for="item in models" :key="item">
-              {{item}}
-          </div>
+      <div class="model-div" v-show="showModel">
+        <div class="model-i">全部车系</div>
+        <div v-for="k in Object.keys(models)" :key="k">
+          <div class="model-c">{{k}}</div>
+          <div class="model-t" v-for="item in models[k]" :key="item.model">{{item.model}}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -28,48 +33,81 @@
   .category-div {
     position: absolute;
     border: 1px solid #ccc;
-    height:770px;
-    width:400px;
-    .letter-div{
-        width: 31px;
-        height:100%;
-        background: #f5f5f5;
-        border-right: 1px solid #ccc;
-        float:left;
-        text-align: center;
-        .letter-i{
-            width: 15px;
-            font-size:12px;
-            line-height: 20px;
-            padding:5px 8px;
-            color:#666;
-            cursor: pointer;
-            &:hover{
-                color:#409EFF;
-                font-weight: 600;
-            }
+    height: 520px;
+    width: 230px;
+    .letter-div {
+      height: 100%;
+      background: #f5f5f5;
+      border-right: 1px solid #ccc;
+      float: left;
+      text-align: center;
+      .letter-i {
+        width: 15px;
+        font-size: 12px;
+        line-height: 12px;
+        padding: 4px 6px;
+        color: #666;
+        cursor: pointer;
+        &:hover {
+          color: #fff;
+          background-color: #409eff;
+          font-weight: 600;
         }
+      }
     }
-    .band-div{
-        float:left;
-        width:180px;
-        height:100%;
-        text-align: left;
-        line-height: 40px;
-        height:40px;
-        .band-c{
-            padding-left:15px;
-            background: #ecf5ff;
-            color:#409EFF;
-            border-right:1px solid #ccc;
-            border-bottom:1px solid #ccc;
+    .band-div {
+      float: left;
+      width: 200px;
+      height: 100%;
+      text-align: left;
+      line-height: 30px;
+      overflow-y: auto;
+      font-size: 15px;
+      .band-c {
+        padding-left: 15px;
+        background: #ecf5ff;
+        color: #409eff;
+        border-right: 1px solid #ccc;
+        border-bottom: 1px solid #ccc;
+        font-weight: 500;
+      }
+      .band-t {
+        padding-left: 18px;
+        border-right: 1px solid #ccc;
+        border-bottom: 1px solid #ccc;
+        &:hover {
+          background: #409eff;
+          color: #fff;
         }
-        .band-t{
-            padding-left:18px;
-            border-right:1px solid #ccc;
-            border-bottom:1px solid #ccc;
+      }
+    }
+    .model-div {
+      width: 200px;
+      position: absolute;
+      left: 200px;
+      height: 100%;
+      border: 1px solid #ccc;
+      background: #fff;
+      top: -1px;
+      text-indent: 1em;
+      .model-i,
+      & > div {
+        line-height: 30px;
+        font-size: 15px;
+        border-bottom: 1px solid #ccc;
+        .model-c {
+          border-bottom: 1px solid #ccc;
+          background: #ecf5ff;
+          color: #409eff;
+          font-weight: 600;
         }
-
+        .model-t {
+          &:hover {
+            background: #409eff;
+            color: #fff;
+          }
+        }
+      }
     }
   }
 }
@@ -79,8 +117,9 @@
 export default {
   data() {
     return {
-      bands:[],
-      models:[],
+      brandList: [],
+      modelList: [],
+      showModel: false,
       letters: [
         "A",
         "B",
@@ -111,18 +150,56 @@ export default {
       ]
     };
   },
-  mounted(){
-    this.getCategory();
+  mounted() {
+    this.getBrand();
   },
-  methods:{
-    getCategory(){
-        this.$http.get('/carCategory/config').then(json=>{
-            if(json.code == '1'){
-                this.bands = json.data;
-            }
-        })
+  computed: {
+    brands() {
+      let brands = {};
+      this.brandList.forEach(item => {
+        if (!brands.hasOwnProperty(item.letter)) {
+          brands[item.letter] = [];
+        }
+        brands[item.letter].push(item.brand);
+      });
+      return brands;
+    },
+    models() {
+      let models = {};
+      this.modelList.forEach(item => {
+        if (!models.hasOwnProperty(item.company)) {
+          models[item.company] = [];
+        }
+        models[item.company].push(item);
+      });
+      return models;
     }
-    
+  },
+  methods: {
+    getBrand() {
+      this.$http.get("/carBrand/list").then(json => {
+        if (json.code == "1") {
+          this.brandList = json.data;
+        }
+      });
+    },
+    changeLetter(item) {
+      let target = this.$refs["car-selector"].querySelector(
+        '.band-c[data-c="' + item + '"]'
+      );
+      if (target) {
+        this.$refs["car-selector"].querySelector(".band-div").scrollTop =
+          target.offsetTop;
+      }
+    },
+    getModel(brand) {
+      this.showModel = true;
+      this.$http.get("/carModel/list", { brand: brand }).then(json => {
+        if (json.code == "1") {
+          this.modelList = json.data;
+        }
+      });
+    }
   }
 };
 </script>
